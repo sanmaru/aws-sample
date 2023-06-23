c5abbbb5ece5cbfc2b1edc8553a6419f0dc9f68a
sanmaru <moni.sanmaru@gmail.com>
2023-06-23 17:06:12 +0900

    swagger 연동 완료

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * @swagger
+ * paths:
+ *   "/":
+ *     get:
+ *       summary: "Hello world"
+ *       response:
+ *         "200":
+ *           description: "이렇게 작성할 수 있습니다."
+ */
 @Injectable()
 export class AppService {
   getHello(): string {
